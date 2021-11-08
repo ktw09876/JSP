32e0d0ceeab35c86dcba0c8e64a402b60e040974
dhqhf <dhqhf@220.71.113.220>
2021-11-08 20:12:07 +0900

    관리자페이지 서밋버튼

---
+++ JSPPrj/src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
@@ -0,0 +1,83 @@
+package com.newlecture.web.controller.admin.notice;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.entity.NoticeView;
+import com.newlecture.web.service.NoticeService;
+
+@WebServlet("/admin/notice/list")
+public class ListController extends HttpServlet {
+	//url이 없으면 404에러(url 오류)
+	//url은 있지만 처리할 수 있는 메서드가 없으면 405에러(메서드 오류)
+	//url도 있고 메서드도 있지만 권한이 없으면 403에러(보안 오류)
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String[] openIds = request.getParameterValues("open-id");
+		String[] delIds = request.getParameterValues("del-id");
+		String cmd = request.getParameter("cmd");
+		
+		switch(cmd) {
+		case "일괄공개":
+			for(String openId : openIds)
+				System.out.printf("open id : %s\n", openId);
+			break;
+		case "일괄삭제":
+			for(String delId : delIds)
+				System.out.printf("del id : %s\n", delId);
+			break;
+		}
+
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//list?f=title&q=a
+		
+		String field_ = request.getParameter("f"); 
+		String query_ = request.getParameter("q"); //일단 사용자가 입력한 값을 임시변수에 담아두고
+		String page_ =  request.getParameter("p");
+		
+		String field = "title"; //기본값
+		if(field_ != null && !field_.equals("")) //사용자가 입력한 값이 null이 아니면
+			field = field_; //그 값을 field에 담아서 전달
+		
+		String query = ""; //기본값
+		if(query_ != null && !query_.equals("")) //사용자가 입력한 값이 null이 아니면
+			query = query_; //그 값을 query에 담아서 전달
+		
+		int page = 1; //기본값
+		if(page_ != null && !page_.equals("")) //사용자가 입력한 값이 null이 아니면
+			page = Integer.parseInt(page_); //그 값을 page에 담아서 전달
+		
+		NoticeService service = new NoticeService();
+		List<NoticeView> list = service.getNoticeList(field, query, page);
+		int count = service.getNoticeCount(field, query);
+		
+		
+		request.setAttribute("list", list);
+		request.setAttribute("count", count);
+		
+		request
+		.getRequestDispatcher("/WEB-INF/view/admin/board/notice/list.jsp")
+		.forward(request, response);
+		
+	}
+}
